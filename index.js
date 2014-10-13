e55e090d6e1417bd0aa4b5f8e77dd22d86515ef2
Alabê Duarte <alabeduarte@gmail.com>
2014-10-13 15:26:06 -0300

    Introduce socket.io, react, and jade (template)

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-var twitter = require('./twitter');
+var twitter = require('./lib/twitter');
+var routes = require('./routes');
 
+var path = require('path');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  var tweets = function (tweet) {
-    return { text: tweet.text };
-  };
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
 
-  twitter.tweets(function (data) {
-    res.send(data.map(tweets));
-  });
+app.get('/', routes.index);
+app.get('/tweets', function(req, res) {
+  var tweets = function (tweet) { return { text: tweet.text }; };
+  twitter.tweets(function (data) { res.send(data.map(tweets)); });
 });
 
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
+var server = app.listen(app.get('port'), function() {
     console.log('Listening on port %d', server.address().port);
+});
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+  console.log('> connected');
+
+  twitter.onTweet(function (tweet) {
+    var data = { text: tweet.text };
+    console.log(">", data.text);
+
+    socket.emit('tweet', data);
+  });
 });
