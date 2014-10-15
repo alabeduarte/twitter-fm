12ef324c418f3dda4d50731a02ed26bc16e080e0
Alabê Duarte <alabeduarte@gmail.com>
2014-10-14 22:25:36 -0300

    Add simple unit test for server side

--- tests/server/twitterSpec.js
+++ tests/server/twitterSpec.js
@@ -1,35 +1,9 @@
 var expect = require('chai').expect;
-var io = require('socket.io-client');
 
-describe('Suite of unit tests', function() {
+describe('Index', function(){
+  it('should return helloworld', function() {
+    var content = 'HelloWorld';
 
-    var socket;
-
-    beforeEach(function(done) {
-        socket = io.connect('http://localhost');
-        socket.on('connect', function() {
-            console.log('worked...');
-        });
-        socket.on('disconnect', function() {
-            console.log('disconnected...');
-        });
-        done();
-    });
-
-    describe('First (hopefully useful) test', function() {
-
-        it('Doing some things with indexOf()', function(done) {
-            expect([1, 2, 3].indexOf(5)).to.be.equal(-1);
-            expect([1, 2, 3].indexOf(0)).to.be.equal(-1);
-            done();
-        });
-
-        it('Doing something else with indexOf()', function(done) {
-            expect([1, 2, 3].indexOf(5)).to.be.equal(-1);
-            expect([1, 2, 3].indexOf(0)).to.be.equal(-1);
-            done();
-        });
-
-    });
-
+    expect(content).to.equal('HelloWorld');
+  });
 });
