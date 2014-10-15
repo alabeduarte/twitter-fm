9ba843f7d7fe8ade1b03b9c770144c08adfb4fbc
Alabê Duarte <alabeduarte@gmail.com>
2014-10-14 23:02:10 -0300

    introduce socket.io as dependency for angular module

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,1 +1,5 @@
-angular.module('twitter-fm', []);
+angular.module('twitter-fm', ['socket.io']);
+
+angular.module('socket.io', []).factory('io', function() {
+  return window.io;
+});
--- public/js/app/twitterListCtrl.js
+++ public/js/app/twitterListCtrl.js
@@ -1,11 +1,15 @@
 var app = angular.module('twitter-fm');
 
-app.controller('TwitterListCtlr', ['$scope', function ($scope) {
-  var socket = io();
+app.controller('TwitterListCtlr', ['$scope', 'io', function ($scope, io) {
+  // var socket = io();
+  var socket = io.connect(window.location.origin);
   $scope.test = '> waiting for tweets...';
 
   socket.on('tweet', function (data) {
     console.log('tweet received: ', data.text);
-    $scope.test = data.text;
+
+    $scope.$apply(function () {
+      $scope.test = data.text;
+    });
   });
 }]);
